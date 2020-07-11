676a5ce6f7f03e53076f85f42fc37135e995cbc4
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-07-11 22:16:47 +0600

    add tests function

--- pkg/python/__init__.py
+++ pkg/python/__init__.py
@@ -3,3 +3,16 @@ __version__ = cpp_module_test.__version__
 
 def echo(value):
     return cpp_module_test.echo(str(value))
+
+def run_test(fname=None):
+    import subprocess
+    import pathlib
+    test_files = []
+    if fname is None:
+        test_files.append(pathlib.Path(__file__).parent / "test_ping.py")
+    elif pathlib.Path(fname).is_file():
+        test_files.append(fname)
+    elif (pathlib.Path(__file__).parent / fname).is_file():
+        test_files.append(pathlib.Path(__file__).parent / fname)
+
+    subprocess.run(["pytest", "-vv", "--capture=tee-sys", "-r", "s"] + test_files)
