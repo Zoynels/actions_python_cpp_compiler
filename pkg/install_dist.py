8f46a5b4cdbfc2c4a232759630c51e71f5a12fd9
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-07-11 22:03:46 +0600

    Create install_dist.py

---
+++ pkg/install_dist.py
@@ -0,0 +1,11 @@
+#-*- coding: utf-8 -*-
+import os, sys
+import subprocess
+
+
+for dp, dn, filenames in os.walk("dist"):
+    for f in filenames:
+        if str(f).lower().endswith(".whl"):
+            subprocess.run(["python", "-m", "pip", "install", os.path.join(dp, f)])
+            sys.exit(0)
+raise RuntimeError("Can't find whl file in dist folder!")
