ce8f3ac483dbd399d04fc9d398746e0cae342fda
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-07-11 23:21:48 +0600

    Update __init__.py

--- pkg/python/__init__.py
+++ pkg/python/__init__.py
@@ -11,13 +11,13 @@ def run_tests(fname=None):
 
     test_files = []
     if fname is None:
-        test_files.append(str(pathlib.Path(__file__).parent / "test_ping.py"))
+        test_files.append(pathlib.Path(__file__).parent / "test_ping.py")
     elif pathlib.Path(fname).is_file():
-        test_files.append(str(fname))
+        test_files.append(fname)
     elif (pathlib.Path(__file__).parent / fname).is_file():
-        test_files.append(str(pathlib.Path(__file__).parent / fname))
+        test_files.append(pathlib.Path(__file__).parent / fname)
 
-
+    test_files = [str(fname) for fname in test_files] # py37 not support pathlib.Path in subprocess
     child = subprocess.Popen(["pytest", "-vv", "--capture=tee-sys", "-r", "s"] + test_files, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     streamdata = child.communicate()
     for ch in [streamdata[0].split(b"\n"), streamdata[1].split(b"\n")]:
