d2a4024acae248246cd893bf13755b2cb6412e62
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-07-11 20:57:24 +0600

    Update setup.py

--- pkg/setup.py
+++ pkg/setup.py
@@ -6,21 +6,31 @@ from distutils.core import setup, Extension
 
 import os, sys, platform
 
+
+def get_fname_path(start_path, fname):
+    for dp, dn, filenames in os.walk(start_path):
+        for f in filenames:
+            if str(f).lower() == fname:
+                return dp
+
+
 cpython_include = []
 if (os.environ.get("pythonLocation", "") != ""):
-    cpython_include.append(os.path.join(os.environ["pythonLocation"], "include"))
+    cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "python.h")
+    cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "pyconfig.h")
 elif (sys.version_info[0] == 3) and  (sys.version_info[1] == 7):
-    cpython_include.append("../external/cpython_3_7_7/Include/")
-    cpython_include.append("../external/cpython_3_7_7/PC/")
+    cpython_include.append("../external/cpython_3_7_7", "python.h")
+    cpython_include.append("../external/cpython_3_7_7", "pyconfig.h")
 elif (sys.version_info[0] == 3) and  (sys.version_info[1] == 8):
-    cpython_include.append("../external/cpython_3_8_3/Include/")
-    cpython_include.append("../external/cpython_3_8_3/PC/")
+    cpython_include.append("../external/cpython_3_8_3", "python.h")
+    cpython_include.append("../external/cpython_3_8_3", "pyconfig.h")
 elif (sys.version_info[0] == 3) and  (sys.version_info[1] == 8):
     raise NotImplementedError(f"Not implemented on {sys.version_info}")
 
 cpython_library = []
 if (os.environ.get("pythonLocation", "") != ""):
-    cpython_library.append(os.path.join(os.environ["pythonLocation"], "libs"))
+    cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "python3.lib") # for windows
+    cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "libpython3.so") # for linux
 elif sys.platform == "linux" or platform == "linux2":
     raise NotImplementedError("Not implemented on Linux")
 elif sys.platform == "darwin":
@@ -53,10 +63,10 @@ ext_modules = [
     Extension(
         "actions_pyhon_cpp_compiler.cpp_module_test", # save to 'folder.filename'
         sources=sources, # files to compile (all cpp files in project)
-        include_dirs=cpython_include + [
+        include_dirs=list(set(cpython_include)) + [
                      "../external/pybind11/include/pybind11/", # instead hard dependency for pybin11 could be used 'pybind11.get_include()' function
                      ],
-        library_dirs=cpython_library,
+        library_dirs=list(set(cpython_library)),
         runtime_library_dirs=[],
         libraries=[],
         language="c++",
