9f3e8623a2292732de5e1cf17070ec8d5a255324
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-07-11 19:11:08 +0600

    remove cpython submodule

--- external/cpython_build_all.bat
+++
@@ -1,8 +0,0 @@
-﻿
-call cpython_3_7_7/PCbuild/build.bat -t Build -p Win32
-call cpython_3_7_7/PCbuild/build.bat -t Build -p x64
-
-call cpython_3_8_3/PCbuild/build.bat -t Build -p Win32
-call cpython_3_8_3/PCbuild/build.bat -t Build -p x64
-
-pause
