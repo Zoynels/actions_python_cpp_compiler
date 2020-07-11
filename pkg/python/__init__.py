3856188a3ed13aee94f7230205dda370ea5321df
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-07-11 22:35:03 +0600

    Update __init__.py

--- pkg/python/__init__.py
+++ pkg/python/__init__.py
@@ -5,8 +5,10 @@ def echo(value):
     return cpp_module_test.echo(str(value))
 
 def run_tests(fname=None):
-    import subprocess
+    import sys
     import pathlib
+    import subprocess
+
     test_files = []
     if fname is None:
         test_files.append(pathlib.Path(__file__).parent / "test_ping.py")
@@ -18,4 +20,4 @@ def run_tests(fname=None):
 
     child = subprocess.Popen(["pytest", "-vv", "--capture=tee-sys", "-r", "s"] + test_files, stdout=subprocess.PIPE)
     streamdata = child.communicate()[0]
-    sys.exit(child.returncode)
+    assert child.returncode == 0
