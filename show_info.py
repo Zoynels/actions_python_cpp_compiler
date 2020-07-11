d24f47bb31c543991494869434e2fd3383a13efb
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-07-11 20:44:14 +0600

    Update python-package.yml

--- show_info.py
+++ show_info.py
@@ -6,7 +6,7 @@ if show:
      print("#" * 100, ": os.walk('.')")
      a=[print(os.path.join(dp, f)) for dp, dn, filenames in os.walk(".") for f in filenames]
 
-show = False
+show = True
 if show:
      print("#" * 100, ": os.walk(os.environ.get('pythonLocation', ''))")
      b=[print(os.path.join(dp, f)) for dp, dn, filenames in os.walk(os.environ.get("pythonLocation", "")) for f in filenames]
