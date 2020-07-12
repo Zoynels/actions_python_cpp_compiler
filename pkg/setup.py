14ea5f160eb989808a5ddfe2e70082709fcb843e
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-07-12 17:47:39 +0600

    remove hardcoded setup.py

--- pkg/setup.py
+++ pkg/setup.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
-from setuptools import setup
-from distutils.core import setup, Extension
-
-import os, sys, platform
 import ast
+import os
+import platform
+import sys
+from distutils.core import Extension, setup
+
+import setuptools  # noqa
 
 
 def get_version():
     """Return version tuple of strings."""
-    import os
     __version__ = ""
     __version_cpp__ = ""
     with open("python/__init__.py") as input_file:
@@ -18,6 +18,7 @@ def get_version():
             if line.startswith("__version__"):
                 __version__ = ast.parse(line).body[0].value.s  # type: ignore
                 break
+
     with open("src/python_onefile.cpp") as input_file:
         for line in input_file:
             if "__version__" in line:
@@ -27,6 +28,14 @@ def get_version():
                 L = "__version_cpp__ " + L[pos:]
                 __version_cpp__ = ast.parse(L).body[0].value.s  # type: ignore
                 break
+    if __version__ == "" and __version_cpp__ == "":
+        raise ValueError(
+            f"Can't detect __version__ of main module and __version_cpp__ of cpp module!")
+    if __version__ == "":
+        raise ValueError(f"Can't detect __version__ of main module!")
+    if __version_cpp__ == "":
+        raise ValueError(f"Can't detect __version_cpp__ of cpp module!")
+
     return __version__, __version_cpp__
 
 
@@ -35,47 +44,49 @@ def get_fname_path(start_path, fname):
         for f in filenames:
             if str(f).lower() == fname:
                 return os.path.abspath(dp)
+    raise ValueError(
+        f"Can't find {fname} in folder {start_path} and its subfolders! " +
+        f"Please check 'pythonLocation' environment variable!")
 
 
 cpython_include = []
 if (os.environ.get("pythonLocation", "") != ""):
     cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "python.h"))
     cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "pyconfig.h"))
-elif (sys.version_info[0] == 3) and  (sys.version_info[1] == 7):
-    cpython_include.append(get_fname_path("../external/cpython_3_7_7", "python.h"))
-    cpython_include.append(get_fname_path("../external/cpython_3_7_7", "pyconfig.h"))
-elif (sys.version_info[0] == 3) and  (sys.version_info[1] == 8):
-    cpython_include.append(get_fname_path("../external/cpython_3_8_3", "python.h"))
-    cpython_include.append(get_fname_path("../external/cpython_3_8_3", "pyconfig.h"))
 else:
-    raise NotImplementedError(f"Not implemented on {sys.version_info}")
+    raise ValueError(
+        "Please set 'pythonLocation' environment variable where Python.h and " +
+        "python3.lib/libpython3.so exist! Files will be searched recursively is this folder.")
 
 cpython_library = []
 if (os.environ.get("pythonLocation", "") != ""):
-    cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "python3.lib")) # for windows
-    cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "libpython3.so")) # for linux
-elif sys.platform == "linux" or platform == "linux2":
-    raise NotImplementedError("Not implemented on Linux")
-elif sys.platform == "darwin":
-    raise NotImplementedError("Not implemented on OS X")
-elif sys.platform == "win32":
-    if platform.architecture()[0] == "32bit":
-        if (sys.version_info[0] == 3) and  (sys.version_info[1] == 7):
-            cpython_library.append("../external/cpython_3_7_7/PCbuild/win32/")
-        elif (sys.version_info[0] == 3) and  (sys.version_info[1] == 8):
-            cpython_library.append("../external/cpython_3_8_3/PCbuild/win32/")
-    if platform.architecture()[0] == "64bit":
-        if (sys.version_info[0] == 3) and  (sys.version_info[1] == 7):
-            cpython_library.append("../external/cpython_3_7_7/PCbuild/amd64/")
-        elif (sys.version_info[0] == 3) and  (sys.version_info[1] == 8):
-            cpython_library.append("../external/cpython_3_8_3/PCbuild/amd64/")
+    if sys.platform == "linux" or platform == "linux2":
+        cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "libpython3.so"))
+    elif sys.platform == "darwin":
+        cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "libpython3.so"))
+    elif sys.platform == "win32":
+        cpython_include.append(get_fname_path(os.path.join(os.environ["pythonLocation"]), "python3.lib"))
+    else:
+        raise RuntimeError(f"Unknown platform: {sys.platform}")
+else:
+    raise ValueError(
+        "Please set 'pythonLocation' environment variable where Python.h and " +
+        "python3.lib/libpython3.so exist! Files will be searched recursively is this folder.")
 
 if sys.platform == "linux" or platform == "linux2":
     extra_compile_args = ["-std=c++17"]
 elif sys.platform == "darwin":
     extra_compile_args = ["-std=c++17"]
 elif sys.platform == "win32":
-    extra_compile_args = ["/std:c++17", "/MP", "/WX-", "/W4", "/sdl", "/Zi", "/Ox", "/O2"] # Visual Studio 2019
+    extra_compile_args = [
+        "/std:c++17",
+        "/MP",
+        "/WX-",
+        "/W4",
+        "/sdl",
+        "/Zi",
+        "/Ox",
+        "/O2"]  # Visual Studio 2019
 
 test_mode = True
 if test_mode:
@@ -84,18 +95,23 @@ if test_mode:
 
 ext_modules = [
     Extension(
-        "actions_python_cpp_compiler.cpp_module_test", # save to 'folder.filename'
-        sources=sources, # files to compile (all cpp files in project)
-        include_dirs=list(set(cpython_include)) + [
-                     "../external/pybind11/include/pybind11/", # instead hard dependency for pybin11 could be used 'pybind11.get_include()' function
-                     ],
+        "actions_python_cpp_compiler.cpp_module_test",  # save to 'folder.filename'
+        sources=sources,  # files to compile (all cpp files in project)
+        include_dirs=list(set(cpython_include)) + ["../external/pybind11/include/pybind11/"],
         library_dirs=list(set(cpython_library)),
         runtime_library_dirs=[],
         libraries=[],
         language="c++",
-        extra_compile_args=extra_compile_args, 
+        extra_compile_args=extra_compile_args,
     ),
 ]
+
+long_description = ""
+long_description_content_type = "text/markdown"
+with open("../readme.md") as readme:
+    long_description = readme.read()
+    long_description_content_type = "text/markdown"
+
 
 setup(
     name="actions_python_cpp_compiler",
@@ -103,8 +119,20 @@ setup(
     url="https://github.com/Zoynels/actions_python_cpp_compiler",
     author="Zoynels",
     author_email="zoynels@gmail.com",
-    description=f"actions_python_cpp_compiler: wrapper version {get_version()[0]} and cpp-module version {get_version()[1]}",
+    description=f"GitHub Actions to build/install/test for c++ module with pybind11:" +
+    f"wrapper version {get_version()[0]} and cpp-module version {get_version()[1]}",
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    license="GNU Affero General Public License v3",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: C++",
+        "Programming Language :: Pybind11",
+        "License :: OSI Approved :: GNU Affero General Public License v3"],
+    keywords="pybind11, c++, github, actions",
     ext_modules=ext_modules,
     packages=["actions_python_cpp_compiler"],
-    package_dir = {"actions_python_cpp_compiler": "lib" , "actions_python_cpp_compiler":"python"}
-)
+    package_dir={"actions_python_cpp_compiler": "python"})
