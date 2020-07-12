d0e246d41591c7c9be90af42e179ffb44f18057c
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-07-12 17:13:09 +0600

    add function to copy files from several directories to root of another directory

--- pkg/util_funcs.py
+++ pkg/util_funcs.py
@@ -6,6 +6,7 @@ import subprocess
 import zipfile
 import json
 import argparse
+import shutil
 
 
 def install_dist(args):
@@ -43,6 +44,27 @@ def extractall(args):
             print(f"Extract all from: {fname}")
             zip_ref.extractall(args.dest_to)
 
+def copyfiles(args):
+    if str(args.func).lower() != "copyfiles":
+        return 0
+
+    if args.pat is None:
+        raise ValueError(f"Unknown 'pat' in args: {args}")
+
+    if args.dest_to is None:
+        raise ValueError(f"Unknown 'dest_to' in args: {args}")
+    
+    for fname in glob.glob(args.pat, recursive=True):
+        print(f"Found: {fname}")
+        if not os.path.isfile(fname):
+            continue
+        fname = os.path.abspath(fname)
+        new_file = os.path.abspath(os.path.join(args.dest_to, os.path.basename(fname)))
+        if not os.path.exists(os.path.dirname(new_file)):
+            os.makedirs(os.path.dirname(new_file))
+        print(f"Copy file from '{fname}' to '{new_file}'")
+        shutil.copyfile(fname, new_file)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--func', help='function name')
@@ -53,5 +75,7 @@ args = parser.parse_args()
 
 # util_funcs.py --func=extractall --pat=artifacts/** --dest_to=dist
 extractall(args)
-# util_funcs.py --func=install_dist --pat=dist/**.whl --pkg_name=actions_python_cpp_compiler
+# util_funcs.py --func=install_dist --pat=dist/**/*.whl --pkg_name=actions_python_cpp_compiler
 install_dist(args)
+# util_funcs.py --func=copyfiles --pat=artifacts/**/*.whl --dest_to=dist
+copyfiles(args)
